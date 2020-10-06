14751acb335843079817e20628959f4d27cd5837
0xN0P <69481028+0xInstintN0P@users.noreply.github.com>
2020-10-06 13:03:00 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace l4d2autobunnyhop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.checkBox1.Location = new System.Drawing.Point(192, 234);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "AutoBunnyHop";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(485, 276);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "L4D2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace l4d2autobunnyhop
+{
+    public partial class Form1 : Form
+    {
+        Mem m = new Mem();
+
+        //DllImport
+        [DllImport("user32.dll")]
+
+        static extern short GetAsyncKeyState(Keys vkey);
+
+
+        string F_JUMP = "client.dll+0x739918";
+        string IN_AIR = "client.dll+0x6C5E80";
+        int result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ici nous appelons le processus - here we call the process
+            int PID = m.GetProcIdFromName("left4dead2");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                Thread BH = new Thread(BHOP) { IsBackground = true };
+                BH.Start();
+            }
+        }
+
+        void BHOP()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.Space) < 0)
+                    {
+                        result = m.ReadInt(IN_AIR);
+                        if (result == 0)
+                        {
+                            // this value
+                            m.WriteMemory(F_JUMP, "int", "5");
+                            Thread.Sleep(35);
+                            m.WriteMemory(F_JUMP, "int", "4");
+                        }
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
